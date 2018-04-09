e3bf71a4e0429e7fd9af8a31481896991096215b
Jay Luker <jay_luker@harvard.edu>
2018-04-09 11:32:55 -0400

    Rewrite of indexer stack and lambda handling. Includes:
    * Dedicated AWS Elasticsearch in dedicated VPC
    * Allows for a single transcript indexer stack to be shared by all
      opencast opsworks clusters within an account
    * Assumes VPC Peering Connections inititated on opsworks cluster creation
    * transcript indexes take the form of `[clustername]-transcripts`
    * lambda function now handles initial index template creation
    * lambda function to be invoked by Opencast as part of workflow
    * caption content now in dxfp/xml
    * using pip-tools for requirements
    * now using common lambda code bucket for packaged function code

--- tasks.py
+++ tasks.py
@@ -1,19 +1,20 @@
 
+import os
 import json
 import boto3
-import arrow
-from time import sleep
+import shutil
 from invoke import task, Exit
 from os import getenv as env
 from dotenv import load_dotenv
 from os.path import join, dirname, exists
+from random import choice
+from ps_environ import Env
 
 load_dotenv(join(dirname(__file__), '.env'))
 
-AWS_PROFILE = env('AWS_PROFILE')
-
-if AWS_PROFILE is not None:
-    boto3.setup_default_session(profile_name=AWS_PROFILE)
+aws_profile = env('AWS_DEFAULT_PROFILE')
+if aws_profile is not None:
+    boto3.setup_default_session(profile_name=aws_profile)
 
 
 def getenv(var, required=True):
@@ -30,15 +31,17 @@ def profile_arg():
     return ""
 
 
-def stack_exists(ctx):
-    cmd = "aws {} cloudformation describe-stacks --stack-name {}" \
-        .format(profile_arg(), getenv('STACK_NAME'))
+def existing_stack(ctx):
+    cmd = ("aws {} cloudformation describe-stacks --stack-name {} " 
+           "--query 'Stacks[0]'"
+           ).format(profile_arg(), getenv('STACK_NAME'))
     res = ctx.run(cmd, hide=True, warn=True, echo=False)
-    return res.exited == 0
+    if res.exited == 0:
+        return json.loads(res.stdout)
 
 
 def s3_zipfile_exists(ctx):
-    cmd = "aws {} s3 ls s3://{}/{}-function.zip" \
+    cmd = "aws {} s3 ls s3://{}/dce-transcript-indexer/{}-function.zip" \
         .format(
             profile_arg(),
             getenv('LAMBDA_CODE_BUCKET'),
@@ -48,44 +51,29 @@ def s3_zipfile_exists(ctx):
     return res.exited == 0
 
 
-def vpc_components(ctx):
-
-    vpc_id = getenv("VPC_ID")
-
-    cmd = ("aws {} ec2 describe-subnets --filters "
-           "'Name=vpc-id,Values={}' "
-           "'Name=tag:aws:cloudformation:logical-id,Values=PrivateSubnet'") \
-        .format(profile_arg(), vpc_id)
-
-    res = ctx.run(cmd, hide=1)
-    subnet_data = json.loads(res.stdout)
-    subnet_id = subnet_data['Subnets'][0]['SubnetId']
-
-    cmd = ("aws {} ec2 describe-security-groups --filters "
-           "'Name=vpc-id,Values={}' "
-           "'Name=tag:aws:cloudformation:logical-id,Values=OpsworksLayerSecurityGroupCommon'") \
-        .format(profile_arg(), vpc_id)
-    res = ctx.run(cmd, hide=1)
-    sg_data = json.loads(res.stdout)
-    sg_id = sg_data['SecurityGroups'][0]['GroupId']
-
-    return subnet_id, sg_id
+def find_cidr_base(ctx):
+    cmd = ("aws {} ec2 describe-vpcs "
+           "--query 'Vpcs[].CidrBlockAssociationSet[].CidrBlock'"
+           ).format(profile_arg())
+    res = ctx.run(cmd, hide=True, echo=False)
+    taken = set([x[:x.rindex('.')] for x in json.loads(res.stdout)])
+    possible = set(["10.1.{}".format(x) for x in range(254)])
+    return choice(list(possible.difference(taken))) + ".0/24"
 
 
 @task
-def create_code_bucket(ctx):
-    """
-    Create the s3 bucket for storing packaged lambda code
-    """
-    code_bucket = getenv('LAMBDA_CODE_BUCKET')
-    cmd = "aws {} s3 ls {}".format(profile_arg(), code_bucket)
-    exists = ctx.run(cmd, hide=True, warn=True)
-    if exists.ok:
-        print("Bucket exists!")
-    else:
-        cmd = "aws {} s3 mb s3://{}".format(profile_arg(), code_bucket)
-        ctx.run(cmd)
-
+def config_fetch(ctx, namespace="default", profile=None):
+    if profile is None:
+        profile = env('AWS_DEFAULT_PROFILE')
+    common_config = Env(service='common', stage=namespace)
+    project_config = Env(service='dce-transcript-indexer', stage=namespace)
+    dotevn_path = join(dirname(__file__), '.env')
+    with open(dotevn_path, 'w') as f:
+        f.write("AWS_DEFAULT_PROFILE={}\n".format(profile))
+        f.write("LAMBDA_CODE_BUCKET={}\n".format(common_config('lambda-code-bucket')))
+        for k, v in project_config.parameter_store_cache.items():
+            var = k.upper().replace('-', '_')
+            f.write("{}={}\n".format(var, v))
 
 @task
 def package(ctx):
@@ -94,19 +82,30 @@ def package(ctx):
     """
 
     build_path = join(dirname(__file__), 'dist')
-    function_path = join(dirname(__file__), 'function.py')
-    zip_path = join(dirname(__file__), 'function.zip')
-    req_file = join(dirname(__file__), 'requirements.txt')
+    shutil.rmtree(build_path, ignore_errors=True)
+    os.makedirs(build_path)
+
+    print("installing dependencies to build path")
+    req_file = join(dirname(__file__), 'function_requirements.txt')
     ctx.run("pip install -U -r {} -t {}".format(req_file, build_path))
-    ctx.run("ln -s -f -r -t {} {}".format(build_path, function_path))
+
+    for asset in ['function.py', 'index_template.json']:
+        asset_path = join(dirname(__file__), asset)
+        ctx.run("ln -s -f -r -t {} {}".format(build_path, asset_path))
+
+    print("packaging zip file")
+    zip_path = join(dirname(__file__), 'function.zip')
     with ctx.cd(build_path):
         ctx.run("zip -r {} .".format(zip_path))
+
+    print("uploading to s3")
     s3_file_name = "{}-function.zip".format(getenv('STACK_NAME'))
-    ctx.run("aws {} s3 cp {} s3://{}/{}".format(
+    ctx.run("aws {} s3 cp {} s3://{}/dce-transcript-indexer/{}".format(
         profile_arg(),
         zip_path,
         getenv("LAMBDA_CODE_BUCKET"),
-        s3_file_name)
+        s3_file_name),
+        echo=True
     )
 
 
@@ -115,13 +114,16 @@ def update_function(ctx):
     """
     Update the function code with the latest packaged zipfile in s3. Note: this will publish a new Lambda version.
     """
+    package(ctx)
+    s3_file_name = "dce-transcript-indexer/{}-function.zip".format(getenv('STACK_NAME'))
     cmd = ("aws {} lambda update-function-code "
-           "--function-name {} --publish --s3-bucket {} --s3-key {}-function.zip"
+           "--function-name {}-function --publish --s3-bucket {} "
+           "--s3-key {}"
            ).format(
         profile_arg(),
         getenv('STACK_NAME'),
         getenv('LAMBDA_CODE_BUCKET'),
-        getenv('STACK_NAME')
+        s3_file_name
     )
     ctx.run(cmd)
 
@@ -138,36 +140,55 @@ def deploy(ctx):
         print("Did you run the `package` command?")
         raise Exit(1)
 
-    create_or_update = stack_exists(ctx) and 'update' or 'create'
-    subnet_id, sg_id = vpc_components(ctx)
+    current_stack = existing_stack(ctx)
+
+    if current_stack is None:
+        create_or_update = "create"
+        cidr_block = find_cidr_base(ctx)
+    else:
+        create_or_update = "update"
+        try:
+            cidr_block = next(
+                x["OutputValue"] for x in current_stack["Outputs"]
+                if x["OutputKey"] == "VpcCidrBlock"
+            )
+        except StopIteration:
+            print("Existing stack doesn't have a cidr block?!?!")
+            raise Exit(1)
 
     cmd = ("aws {} cloudformation {}-stack "
            "--stack-name {} "
            "--capabilities CAPABILITY_NAMED_IAM "
            "--template-body file://{} "
-           "--tags Key=Project,Value=MH Key=OU,Value=DE "
+           "--tags Key=Project,Value=MH Key=OU,Value=DE Key=TranscriptIndexer,Value=1 "
            "--parameters "
-           "ParameterKey=VpcSubnetId,ParameterValue='{}' "
-           "ParameterKey=VpcSecurityGroupId,ParameterValue='{}' "
+           "ParameterKey=CidrBlock,ParameterValue='{}' "
            "ParameterKey=LambdaCodeBucket,ParameterValue='{}' "
-           "ParameterKey=ElasticsearchHost,ParameterValue='{}' "
-           "ParameterKey=ElasticsearchTranscriptIndex,ParameterValue='{}' "
            "ParameterKey=NotificationEmail,ParameterValue='{}' "
-           "ParameterKey=TranscriptBucket,ParameterValue='{}' "
+           "ParameterKey=ElasticsearchInstanceType,ParameterValue='{}' "
            ).format(
         profile_arg(),
         create_or_update,
         getenv("STACK_NAME"),
         template_path,
-        subnet_id,
-        sg_id,
+        cidr_block,
         getenv('LAMBDA_CODE_BUCKET'),
-        getenv('ES_HOST'),
-        getenv('ES_TRANSCRIPT_INDEX'),
         getenv('NOTIFICATION_EMAIL'),
-        getenv('TRANSCRIPT_BUCKET')
+        getenv('ES_INSTANCE_TYPE')
     )
+
+    res = ctx.run(cmd, warn=True, hide=True)
+    if res.exited != 0 and "No updates" in res.stderr:
+        print("Stack is up-to-date!")
+        return
+    elif res.exited != 0:
+        raise Exit(res.stderr)
+
+    print("Waiting for deployment/update to complete...")
+    cmd = ("aws --profile test cloudformation wait stack-{}-complete "
+           "--stack-name {}").format(create_or_update, getenv('STACK_NAME'))
     ctx.run(cmd)
+    print("Done")
 
 
 @task
@@ -179,101 +200,39 @@ def delete(ctx):
            "--stack-name {}").format(profile_arg(), getenv('STACK_NAME'))
     if input('are you sure? [y/N] ').lower().strip().startswith('y'):
         ctx.run(cmd)
+        print("Waiting for deletion to complete...")
+        cmd = ("aws --profile test cloudformation wait stack-delete-complete "
+               "--stack-name {}").format(getenv('STACK_NAME'))
+        ctx.run(cmd)
+        print("Done")
     else:
         print("not deleting stack")
 
 
 @task
-def setup_bucket_notifications(ctx):
-    """
-    Create the bucket notification configuration that allows the s3
-    transcripts bucket to invoke our lambda function for new objects
-    """
-    cmd = "aws {} lambda get-function --function-name {}" \
-        .format(profile_arg(), getenv('STACK_NAME'))
-    res = ctx.run(cmd, hide=1)
-    function_data = json.loads(res.stdout)
-    function_arn = function_data['Configuration']['FunctionArn']
+def init_index_template(ctx):
 
-    s3 = boto3.client('s3')
-
-    config = {
-        'Id': "{}-bucket-notifications".format(getenv('STACK_NAME')),
-        'LambdaFunctionArn': function_arn,
-        'Events': ['s3:ObjectCreated'],
-        'Filter': {
-            'Key': {
-                'FilterRules': [
-                    {
-                        'Name': 'suffix',
-                        'Value': 'json'
-                    }
-
-                ]
-            }
-        }
-    }
-
-
-def create_event(key):
-    return json.dumps({
-      "Records": [
-        {
-          "s3": {
-            "bucket": {
-              "name": getenv('TRANSCRIPT_BUCKET')
-            },
-            "object": {
-              "key": key
-            }
-          }
-        }
-      ]
-    })
+    event_payload = '{ "init_index_template": true }'
+    cmd = ("aws {} lambda invoke --function-name {}-function "
+           "--invocation-type RequestResponse --log-type None "
+           "--payload '{}' /dev/null"
+           ).format(profile_arg(), getenv("STACK_NAME"), event_payload)
+    ctx.run(cmd)
 
 
 @task
-def reindex(ctx, key=None, start=None, end=None):
+def ssh_tunnel(ctx, opsworks_stack):
+    cmd = ("aws {} ec2 describe-instances --output text "
+           "--filters \"Name=tag:opsworks:stack,Values={}\" "
+           "--query \"Reservations[].Instances[?Tags[?Key=='opsworks:instance' && contains(Value, 'admin1')]].PublicIpAddress\" "
+           ).format(profile_arg(), opsworks_stack)
+    instance_ip = ctx.run(cmd, hide=True).stdout.strip()
+    # get ES endpoint
+    cmd = ("aws {} cloudformation describe-stacks --stack-name {} "
+           "--query \"Stacks[].Outputs[?OutputKey=='DomainEndpoint'].OutputValue\" "
+           "--output text"
+           ).format(profile_arg(), env('STACK_NAME'))
+    es_endpoint = ctx.run(cmd, hide=True).stdout.strip()
+    print("ssh -N -f -L 9200:{}:443 {}".format(es_endpoint, instance_ip))
 
-    def generate_events(start, end):
-        s3 = boto3.client('s3')
-
-        params = {
-            'Bucket': getenv('TRANSCRIPT_BUCKET'),
-            'PaginationConfig': {
-                'MaxKeys': 10000,
-                'PageSize': 100
-            }
-        }
-        paginator = s3.get_paginator('list_objects')
-        resp_itr = paginator.paginate(**params)
-
-        for resp in resp_itr:
-            for obj in resp['Contents']:
-                if not obj['Key'].endswith('.json'):
-                    continue
-                if start is not None and obj['LastModified'] < start:
-                    continue
-                if end is not None and obj['LastModified'] > end:
-                    continue
-                yield create_event(obj['Key'])
-
-    if key is not None:
-        events = [create_event(key)]
-    else:
-
-        if start is not None:
-            start = arrow.get(start).datetime
-        if end is not None:
-            end = arrow.get(end).datetime
-        else:
-            end = arrow.utcnow().datetime
-
-        events = generate_events(start, end)
-
-    for event in events:
-        cmd = "aws {} lambda invoke --function-name {} " "--payload '{}' reindex.out" \
-            .format(profile_arg(), getenv('STACK_NAME'), event)
-        ctx.run(cmd)
-        sleep(1)
 
